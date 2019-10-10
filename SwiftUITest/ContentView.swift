5f07aecc3a9360376ed98f439f427b74bc53c95e
mob-GitHub <halu.nomi@gmail.com>
2019-10-10 22:54:51 +0900

    Initial Commit

---
+++ SwiftUITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by 野見山晴輝 on 2019/10/10.
+//  Copyright © 2019 Haruki Nomiyama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
