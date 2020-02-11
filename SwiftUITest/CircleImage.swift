4f0af9a98523e02305c8b1108b985e59670f57d1
mob-GitHub <halu.nomi@gmail.com>
2020-02-11 15:41:10 +0900

    Creating and Combining Views

---
+++ SwiftUITest/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  SwiftUITest
+//
+//  Created by 野見山晴輝 on 2020/02/10.
+//  Copyright © 2020 Haruki Nomiyama. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .frame(minWidth: 0, maxWidth: .infinity, maxHeight: 250)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
